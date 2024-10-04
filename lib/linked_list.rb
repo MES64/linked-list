a96dab83d2af077abdddec5d8bf89c2b694380f9
Mark Skelton <markskelton10@gmail.com>
2024-10-04 13:52:24 +0100

    Create append method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'node'
+
 # LinkedList class implements a linked list
 # Includes information about the head, tail, and size of the list
 # Methods: head, tail, size
@@ -10,5 +12,18 @@ class LinkedList
     @head = nil
     @tail = nil
     @size = 0
+  end
+
+  def append(value)
+    node_to_add = Node.new(value)
+
+    if size.zero?
+      @head = node_to_add
+    else
+      @tail.next_node = node_to_add
+    end
+    @tail = node_to_add
+
+    @size += 1
   end
 end
--- main.rb
+++ main.rb
@@ -5,15 +5,9 @@ require_relative 'lib/node'
 
 linked_list = LinkedList.new
 p linked_list
-
-p linked_list.head
-p linked_list.tail
-p linked_list.size
-
-puts ''
-
-node2 = Node.new(7)
-node1 = Node.new(6, node2)
-p node1
-p node2
-p Node.new
+linked_list.append(1)
+p linked_list
+linked_list.append(2)
+p linked_list
+linked_list.append(3)
+p linked_list
